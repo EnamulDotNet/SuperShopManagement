2290964c83367325cb8381899ea7e2823520db8a
MD. ENAMUL HAQUE <EnamulDotNet@users.noreply.github.com>
2016-09-29 17:05:41 +0600

    add class in models

---
+++ SuperShopManagement/SuperShopManagement/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperShopManagement.Models
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+        public  string AdminUserName { get; set; }
+
+        public  string AdminPassword { get; set; }
+
+    }
+}
---
+++ SuperShopManagement/SuperShopManagement/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperShopManagement.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductQty { get; set; }
+        public double ProductBuyPrice { get; set; }
+        public double ProductSellPrice { get; set; }
+        public int CatagoryId { get; set; }
+
+    }
+}
