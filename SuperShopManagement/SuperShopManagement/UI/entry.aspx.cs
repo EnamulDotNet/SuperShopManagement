9825e00fb9e3054b7d7642ec443f7d30b2c2c6dd
MD. ENAMUL HAQUE <EnamulDotNet@users.noreply.github.com>
2016-10-04 19:47:02 +0600

    entry page update

--- SuperShopManagement/SuperShopManagement/UI/entry.aspx.cs
+++ SuperShopManagement/SuperShopManagement/UI/entry.aspx.cs
@@ -16,10 +16,10 @@ namespace SuperShopManagement.UI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["Sid"] == null)
-            {
-                Response.Redirect("index.aspx");
-            }
+            //if (Session["Sid"] == null)
+            //{
+            //    Response.Redirect("index.aspx");
+            //}
             if (!IsPostBack)
             {
                 string constr = ConfigurationManager.ConnectionStrings["SuperShopDbConnection"].ConnectionString;
